a288f8d1a9a05d4724011391c27efdefaa77e0c8
ALI RAZA KHAN <161626332+ale55777@users.noreply.github.com>
2024-06-15 11:30:17 +0500

    Add files via upload

---
+++ CurrencyConverter/CC.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer"
+async function main() {
+    const S = await inquirer.prompt([{
+        type: "input",
+        name: "Val1",
+        message: "Enter the source amount"
+    }]);
+
+    const I = await inquirer.prompt([{
+        type: "input",
+        name: "Val4",
+        message: "Enter the Initial currency"
+    }]);
+
+    const T = await inquirer.prompt([{
+        type: "input",
+        name: "Val2",
+        message: "Enter the Target currency"
+    }]);
+
+    const E = await inquirer.prompt([{
+        type: "input",
+        name: "Val2",
+        message: "Enter the Exchange Rate to convert from source currency to target currency"
+    }]);
+console.log("\n");
+console.log("INITIAL AMOUNT "+S.Val1);
+const initialCurrency: string = I.Val4;
+console.log(`The amount is in : ${initialCurrency}`);
+const targetCurrency: string = T.Val2;
+console.log(`The amount is converted in : ${targetCurrency}`);
+console.log("CONVETRTED AMOUNT:"+S.Val1*E.Val2);
+
+}
+main();
